acf41031ff6f55bb6c5f198764ab2447a4a91606
LilianaDJ <isc-lili@outlook.com>
2021-04-07 13:11:36 -0500

    llamada a la api

---
+++ api-nasa/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <div className = "navbar navbar-dark bg-dark" >
+            <h1 className = "navbar-brand" >MARS ROVER PICTURES</h1>
+        </div>
+    )
+}
+export default Navbar
